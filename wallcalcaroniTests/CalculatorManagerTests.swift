ade19283fe2beab1804bd811455bf6a57ef4bd8e
Georgi Malkhasyan <malkhasyangeorge@gmail.com>
2020-08-14 18:36:08 -0700

    Test Inserting Value

--- wallcalcaroniTests/CalculatorManagerTests.swift
+++ wallcalcaroniTests/CalculatorManagerTests.swift
@@ -27,18 +27,24 @@ class CalculatorManagerTests: XCTestCase {
     func testInit_WhenGivenValues_TakesValues() {
         let calcManager = CalculationManager(valueA: 10, valueB: 20, currentOperand: .multiply, valueEntryArray: [])
         XCTAssertEqual(calcManager.valueA, 10)
+        XCTAssertEqual(calcManager.valueB, 20)
     }
 
-//    func testExample() throws {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
+
+    func testValues_AreIniitialyZero() {
+        XCTAssertEqual(sut.valueA, 0)
+        XCTAssertEqual(sut.valueB, 0)
+    }
+    
+    
+    func testvalues_Inserting_ChangesValueA(){
+        sut.insert(value: 10)
+        XCTAssertEqual(sut.valueA, 10)
+    }
+    
+    
+    func testOperand_isNil(){
+        XCTAssertNil(sut.currentOperand)
+    }
 
 }
